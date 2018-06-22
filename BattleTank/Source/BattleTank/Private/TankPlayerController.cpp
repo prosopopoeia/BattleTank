2f29a2b8d7ab91624988685b9f3505a6206fd3b0
Isaacs <isaacsj@msudenver.edu>
2018-06-21 22:02:30 -0600

    add AI component for tank, added addt'l tank

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -1,10 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankPlayerController.h"
-
+#include "BattleTank.h"
 
 
 ATank * ATankPlayerController::GetControlledTank() const
 {
+	UE_LOG(LogTemp, Warning, TEXT("A tank made"));
 	return Cast<ATank> (GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto controlledtank = GetControlledTank();
+	UE_LOG(LogTemp, Warning, TEXT("begin!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"));
+	if (!controlledtank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("NOT POSSESSING TANK"));
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("is possesing tank, !"));
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Player controller begin playyyyyy!"));
+
 }
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -2,7 +2,7 @@
 
 #pragma once
 
-#include "BattleTank.h"
+#include "Tank.h"
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
 #include "TankPlayerController.generated.h"
@@ -16,7 +16,9 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 
 public:
-		ATank * GetControlledTank() const;
+		ATank* GetControlledTank() const;
+
+		void BeginPlay() override;
 	
 	
 };
